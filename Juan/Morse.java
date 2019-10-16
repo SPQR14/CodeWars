0eb4cabe16e4cc74568d6f4a61b08af8e236cb55
juantemich <juanftp10@gmail.com>
2019-10-16 17:24:27 -0500

    Corregir Morse

--- Juan/Morse.java
+++ Juan/Morse.java
@@ -2,52 +2,69 @@ import java.util.StringTokenizer;
 
 public class Morse {
 	static String final_str = "";
-	static String stack_str = "";
-	static String sub1 = "";
-	static String sub2 = "";
-	static int n_spaces = 0;
+	static String temp_str = "";
+	static String temp = "";
+	static String clean_str = "";
+	static boolean hasText = true;
 	public static void main(String...Args) {
-		String text_morse = " .... . -.--   .--- ..- -.. . ...---...";
-		System.out.println("Enviando: "+text_morse);
-		System.out.println("Respuesta: "+decode(text_morse));
+		String text_morse = " ... --- ... -.-.-- - .... . --.- ..- .. -.-. -.- -... .-. --- .-- -. ..-. --- -..- .--- ..- - .--. ... --- ...- . .-. - .... . .- .. .- - .. -.-- - .. --- -. .-.-.-";
+		print("Dato de entrada: "+text_morse);
+		print("Resultado: "+decode(text_morse));
 	}
 
-	public static String decode(String morseCode) {
-        if(morseCode.contains(" ")) {
-			/*StringTokenizer one_space = new StringTokenizer(morseCode);
-			
-			while(one_space.hasMoreTokens()) {
-				stack_str = one_space.nextToken();
-				System.out.println(stack_str);
-				final_str += (alphabet(stack_str) != null ? alphabet(stack_str) : "");
-			}*/
-
-			for(int x = 0; x < morseCode.length(); x++) {
-				if(morseCode.charAt(x) != ' ') {
-					stack_str += morseCode.charAt(x);
-
-					if(n_spaces == 1 && !stack_str.isEmpty()) {
-						System.out.println(stack_str);
-						/*Sacar el stack y pasar a final sin espaciado de palabra*/
-						final_str += (alphabet(stack_str) != null ? alphabet(stack_str) : "");
-					} else if(n_spaces > 1 && !stack_str.isEmpty()) {
-						System.out.println(stack_str);
-						/*Sacar el stack y pasar a final con espaciado de palabra*/
-						final_str += (alphabet(stack_str) != null ? alphabet(stack_str)+" " : "");
-						n_spaces = 0;
-					} else {
-						n_spaces = 0;
+	public static String decode(String morse_code) {
+		//Si el String no está vacío
+		if(!morse_code.isEmpty()) {
+			//Limpiar el String
+			clean_str = cleanString(morse_code);
+			if(clean_str.contains(" ")) {
+				//Separación de palabras si las hay
+				if(clean_str.indexOf("  ") > -1) {
+					//Hay palabras y códigos
+					while (hasText) {
+						if(clean_str.indexOf("  ") > -1) {
+							temp = clean_str.substring(0, clean_str.indexOf("  "));
+							clean_str = clean_str.substring(clean_str.indexOf("  ")+2, clean_str.length());
+							final_str += decodeLine(temp)+" ";
+						} else if((clean_str.indexOf("  ") == -1) && (clean_str.length() > 0)) {
+							temp = clean_str;
+							clean_str = "";
+							final_str += decodeLine(temp);
+							hasText = false;
+						} else {
+							hasText = false;
+						}
 					}
+					final_str = final_str.trim();
 				} else {
-					/*Encuentra un espacio*/
-					n_spaces++;
+					//No hay palabras, solo códigos
+					final_str = decodeLine(clean_str);
 				}
+			} else {
+				final_str = alphabet(clean_str);
 			}
-
 			return final_str;
 		} else {
-			return alphabet(morseCode);
+			return final_str;
 		}
+	}
+
+	public static String decodeLine(String str_line) {
+		StringTokenizer lines = new StringTokenizer(str_line);
+		temp_str = ""; temp = "";
+		while(lines.hasMoreTokens()) {
+			temp = lines.nextToken();
+			temp_str += (alphabet(temp) != null ? alphabet(temp) : null);
+		}
+		return temp_str;
+	}
+
+	public static void print(Object data) {
+		System.out.println(data);
+	}
+
+	public static String cleanString(String str_input) {
+		return str_input.trim().replaceAll("\\s{2,}", "  ");
 	}
 	
 	public static String alphabet(String morseText) {
@@ -105,6 +122,36 @@ public class Morse {
 			return "Y";
 		} else if(morseText.equals("--..")) {
 			return "Z";
+		} else if(morseText.equals("-----")) {
+			return "0";
+		} else if(morseText.equals(".----")) {
+			return "1";
+		} else if(morseText.equals("..---")) {
+			return "2";
+		} else if(morseText.equals("...--")) {
+			return "3";
+		} else if(morseText.equals("....-")) {
+			return "4";
+		} else if(morseText.equals(".....")) {
+			return "5";
+		} else if(morseText.equals("-....")) {
+			return "6";
+		} else if(morseText.equals("--...")) {
+			return "7";
+		} else if(morseText.equals("---..")) {
+			return "8";
+		} else if(morseText.equals("----.")) {
+			return "9";
+		} else if(morseText.equals(".-.-.-")) {
+			return ".";
+		} else if(morseText.equals("-.-.--")) {
+			return ",";
+		} else if(morseText.equals("..--..")) {
+			return "?";
+		} else if(morseText.equals(".-..-.")) {
+			return "\"";
+		} else if(morseText.equals("--..--")) {
+			return "!";
 		} else {
 			return null;
 		}
