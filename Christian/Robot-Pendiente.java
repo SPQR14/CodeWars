aecdb298afbf4dbdb6d203f90822d4d6896ab564
christian.alonso <christian.alonso@bbva.com>
2020-01-23 17:05:56 -0600

    Pendiente Robot

---
+++ Christian/Robot-Pendiente.java
@@ -0,0 +1,5 @@
+/*
+Welcome! To the world of robots. You need to create your own simple robot and make your robot tell you the reverse of every word you tell to the robot.Eg. If you say "hello" to your robot, your robot will say "olleh". That's it! Simple Enjoy!!
+
+Link: https://www.codewars.com/kata/56576f15a00ccb1c8f000088/train/java
+*/
