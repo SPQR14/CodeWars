9b864118cf1377711d9c9386b8b17694f2e03d31
albertoisaac.pico <albertoisaac.pico@bbva.com>
2020-03-09 13:52:14 -0600

    LOLAZO

---
+++ SPQR14/Python/remove_specific.py
@@ -0,0 +1,18 @@
+# Enlace al kata:
+# https://www.codewars.com/kata/581bb3c1c221fb8e790001ef/train/python
+
+import numpy as np
+
+def select_subarray(arr):
+    subsum = 0
+    subprod = 0
+    aux = 0
+    indexes = []
+    q = []
+    for i in range(len(arr)):
+        aux = arr.pop(i)
+        print(arr)
+        
+    return arr
+    
+    print(select_subarray([1, 23, 2, -8, 5]))
